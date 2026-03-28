2d37aea65c1ca93338ceec1e8fcf005923be76a3
Minh <dangminh214@gmail.com>
2026-03-28 22:29:21 +0100

    add lru

---
+++ lru.cpp
@@ -0,0 +1,3 @@
+#include "lru.h"
+
+// TODO: implement this class
--- replacer.h
+++ replacer.h
@@ -1,13 +1,14 @@
+#include <cstddef>
 #ifndef REPLACER_H
 #define REPLACER_H
 
-class Repalcer {
+class Replacer {
 public: 
-    virtual bool evictVictim() = 0;
-    virtual void  pin(int, int) = 0;
-    virtual unpin (int, int) = 0;
-    virtual size_t size() = 0;
+    virtual bool victim(int& frame_id) = 0;
+    virtual void pin(int, int) = 0; // TODO, change this param into frame_id
+    virtual void unpin (int, int) = 0;
+    virtual size_t Size() = 0;
     virtual ~Replacer() = default;
-}
+};
 
 #endif // REPLACER_H
