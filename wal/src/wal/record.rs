28bf55ce46f4c30ed33b5e9f9c31444c7e5a6964
minhdangnguyenn <dangminh214@gmail.com>
2026-04-28 02:53:02 +0200

    Stop tracking target directory

---
+++ wal/src/wal/record.rs
@@ -0,0 +1,4 @@
+pub struct WalRecord {
+    pub data: Vec<u8>,
+    pub timestamp: std::time::SystemTime,
+}
---
+++ wal/target/debug/deps/wal-2fb6a458e806e6a1.d
@@ -0,0 +1,5 @@
+/home/minh/repo/database-system/wal/target/debug/deps/wal-2fb6a458e806e6a1.d: src/main.rs
+
+/home/minh/repo/database-system/wal/target/debug/deps/libwal-2fb6a458e806e6a1.rmeta: src/main.rs
+
+src/main.rs:
